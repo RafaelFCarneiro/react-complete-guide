18206da7383c7d2fb100b6cd3f6ef824f17b7dd5
rafael.carneiro <Rafael_Carneiro@gap.com>
2022-01-26 15:23:10 -0300

    cleanup for next section

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test("renders let's get started", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Let's get started/i);
-  expect(linkElement).toBeInTheDocument();
-});
