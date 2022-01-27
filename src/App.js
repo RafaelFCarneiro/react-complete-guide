470f982a695f6274c201aee7de9861c15b830a14
rafael.carneiro <Rafael_Carneiro@gap.com>
2022-01-27 17:22:10 -0300

    clean up and setup for the new section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Fragment, useState } from 'react';
-import AddUser from './components/User/AddUser';
-import UsersList from './components/User/UsersList';
+import React, { useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const addUserHandler = (newUser) =>
-    setUsers((prevUsers) => [...prevUsers, newUser]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
 
   return (
-    <Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={users} />
-    </Fragment>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/components/Helpers/Wrapper.js
+++
@@ -1,3 +0,0 @@
-// It's not necessary, can use Fragment - It's just to understand how Fragment works
-const Wrapper = (props) => props.children;
-export default Wrapper;
