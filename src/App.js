e4646b6ce0fe3354748037edf583cafe5641b39b
rafael.carneiro <Rafael_Carneiro@gap.com>
2022-02-02 13:37:36 -0300

    cleanup and steup for next section

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import { Fragment } from 'react';
-import { useSelector } from 'react-redux';
-
-import Counter from './components/Counter';
-import Header from './components/Header';
-import Auth from './components/Auth';
-import UserProfile from './components/UserProfile';
-
 function App() {
-  const isAuth = useSelector(({ auth }) => auth.isAuthenticated);
   return (
-    <Fragment>
-      <Header />
-      {!isAuth && <Auth />}
-      {isAuth && <UserProfile />}
-      <Counter />
-    </Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
