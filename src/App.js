5bbe8aff1557673bad40993b0570ba3a10dda2fd
rafael.carneiro <Rafael_Carneiro@gap.com>
2022-02-03 14:14:38 -0300

    Section 20: setup for time to practice

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import { Redirect, Route, Switch } from 'react-router-dom';
-import MainHeader from './components/MainHeader';
-import Products from './pages/Products';
-import Welcome from './pages/Welcome';
-import ProductDetail from './pages/ProductDetail';
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome'/>
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/products" exact>
-            <Products />
-          </Route>
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
+
     </div>
   );
 }
